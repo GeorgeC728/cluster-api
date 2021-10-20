d50b156c70edb2305748ef72620ca83004373647
George Desktop <georgecaws@gmail.com>
2021-10-20 23:17:29 +0100

    started the create server call

--- src/api.py
+++ src/api.py
@@ -8,6 +8,10 @@ api = Flask(__name__)
 # Register healthcheck call
 api.register_blueprint(healthcheck_call)
 
+@api.route("/api/v1/server/<id>/create")
+def create_server(id):
+    return({"id": id})
+
 # Start app
 if __name__ == "__main__":
     api.run(host = "0.0.0.0", port = 50)
