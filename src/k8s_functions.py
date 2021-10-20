158d595a2c7295943f46d6c072b5f0d4c29d8934
GeorgeCaws <georgecaws@gmail.com>
2021-10-21 00:35:26 +0100

    param pod spec function

--- src/k8s_functions.py
+++ src/k8s_functions.py
@@ -39,3 +39,26 @@ def param_volume_claim(id, size_gb):
     )
     # Return the volume claim
     return(volume_claim)
+
+def param_pod_spec(id, game, ram_gb):
+    pod = client.V1PodSpec(
+        termination_grace_period_seconds = 180,
+        containers = [
+            client.V1Container(
+                name = "minecraft-container-id-" + id,
+                image = get_image_name(game),
+                volume_mounts = [
+                    client.V1VolumeMount(
+                        name = "minecraft-pvc-" + id,
+                        mount_path = "/data/server"
+                    )
+                ],
+                env = [
+                    client.V1EnvVar(
+                        name = "MEMORY",
+                        value = str(ram_gb * 1024)
+                    )
+                ]
+            )
+        ]
+    )
